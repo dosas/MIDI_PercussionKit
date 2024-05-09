003ab114df8ba6d01ebd051d0092725da34e706d
dosas <dosas@github.com>
2024-05-09 12:19:27 +0200

    Add debug macro

--- debug.h
+++ debug.h
@@ -3,18 +3,20 @@
 #pragma once
 
 // MIDI vs debugging mode
-#define USE_MIDI 1
+#ifdef DEBUG
+#define USE_MIDI 0
 
-#if USE_MIDI
-#define PRINT(s, v)   {}
-#define PRINTX(s, v)  {}
-#define PRINTS(s)     {}
-
-#define SERIAL_RATE 31250
-#else
 #define PRINT(s, v)   { Serial.print(F(s)); Serial.print(v); }
 #define PRINTX(s, v)  { Serial.print(F(s)); Serial.print(F("0x")); Serial.print(v, HEX); }
 #define PRINTS(s)     { Serial.print(F(s)); }
 
 #define SERIAL_RATE 57600
+#else
+#define USE_MIDI 1
+
+#define PRINT(s, v)   {}
+#define PRINTX(s, v)  {}
+#define PRINTS(s)     {}
+
+#define SERIAL_RATE 31250
 #endif // USE MIDI
